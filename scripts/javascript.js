394bbc30ee6e3da15ec75eaf6e7f05b12e172137
Jimmy Givans <jauyoma.dev@gmail.com>
2023-10-11 12:20:44 +0300

    Display the running score
    - Add playerScore and computerScore to keep track of each character's
      current score
    - Create new score DOM element to handle the display of these scores

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -14,6 +14,9 @@ function getComputerChoice() {
     }
 }
 
+let playerScore = 0;
+let computerScore = 0;
+
 function playRound(playerSelection, computerSelection) {
     playerSelection.toLowerCase();
 
@@ -22,23 +25,29 @@ function playRound(playerSelection, computerSelection) {
     }
 
     if (playerSelection === "rock" && computerSelection === "paper") {
+        ++computerScore;
         return "You Lose! Paper beats Rock";
     }
     else if (playerSelection === "paper" && computerSelection === "rock") {
+        ++playerScore;
         return "You Win! Paper beats Rock";
     }
 
     if (playerSelection === "rock" && computerSelection === "scissors") {
+        ++playerScore;
         return "You Win! Rock beats Scissors";
     }
     else if (playerSelection === "scissors" && computerSelection === "rock") {
+        ++computerScore;
         return "You Lose! Rock beats Scissors";
     }
 
     if (playerSelection === "scissors" && computerSelection === "paper") {
+        ++playerScore;
         return "You Win! Scissors beats Paper";
     }
     else if (playerSelection === "paper" && computerSelection === "scissors") {
+        ++computerScore;
         return "You Lose! Scissors beats Paper";
     }
 }
@@ -46,23 +55,28 @@ function playRound(playerSelection, computerSelection) {
 function game() {
     const body = document.querySelector("body");
     const div = document.createElement("div");
+    const score = document.createElement("div");
 
     const rockButton = document.querySelector(".btn-rock");
     rockButton.addEventListener("click", () => {
         div.textContent = playRound("rock", getComputerChoice());
+        score.textContent = `Player Score = ${playerScore}, Computer Score = ${computerScore}`;
     });
 
     const paperButton = document.querySelector(".btn-paper");
     paperButton.addEventListener("click", () => {
-        div.textContent = playRound("paper", getComputerChoice()); 
+        div.textContent = playRound("paper", getComputerChoice());
+        score.textContent = `Player Score = ${playerScore}, Computer Score = ${computerScore}`;
     });
 
     const scissorsButton = document.querySelector(".btn-scissors");
     scissorsButton.addEventListener("click", () => {
         div.textContent = playRound("scissors", getComputerChoice());
+        score.textContent = `Player Score = ${playerScore}, Computer Score = ${computerScore}`;
     });
 
     body.appendChild(div);
+    body.appendChild(score);
 }
 
 game();
