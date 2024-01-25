bb5d292efa8dbab1967df242caf20a193422ef88
Jimmy Givans <jauyoma.dev@gmail.com>
2024-01-25 17:08:33 +0300

    Rename player to user
    - It makes more semantic sense as both the user and the computer
    are players of the game.

--- src/WinConditions.js
+++ src/WinConditions.js
@@ -2,22 +2,22 @@
  * Determine if the player won a round of the game
  * @param {string} player The player's move
  * @param {string} computer The computer's move
- * @returns True if the player won the round, and false otherwise
+ * @returns True if the user won the round, and false otherwise
  */
-export const PlayerHasWon = (player, computer) => {
+export const PlayerHasWon = (user, computer) => {
   return (
-    (player === "paper" && computer === "rock") ||
-    (player === "rock" && computer === "scissors") ||
-    (player === "scissors" && computer === "paper")
+    (user === "paper" && computer === "rock") ||
+    (user === "rock" && computer === "scissors") ||
+    (user === "scissors" && computer === "paper")
   );
 };
 
 /**
  * Determine if the computer won a round of the game
- * @param {string} player The player's move
+ * @param {string} user The user's move
  * @param {string} computer The computer's move
  * @returns True if the computer won, and false otherwise
  */
-export const ComputerHasWon = (player, computer) => {
-  return !PlayerHasWon(player, computer);
+export const ComputerHasWon = (user, computer) => {
+  return !PlayerHasWon(user, computer);
 };
