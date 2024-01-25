fea4720d5d07630ead6f42f039c9d1ec1812e5bc
Jimmy Givans <jauyoma.dev@gmail.com>
2024-01-25 17:07:27 +0300

    Use module PlayRound
    - Delete unused module imports
    - Delete unused function playRound

--- src/app.js
+++ src/app.js
@@ -1,34 +1,7 @@
 "use strict";
 
 import { GetComputerChoice } from "./GetComputerChoice.js";
-import { PlayerHasWon, ComputerHasWon } from "./WinConditions.js";
-
-/**
- * Determine which player has won a round of the game, and return a string congratulating the winner
- * @param {string} player The player's move during this round of the game
- * @param {string} computer The computer's move during this round of the game
- * @returns A string congratulating the winner of the round
- */
-function playRound(player, computer) {
-  player = player.toLowerCase();
-
-  if (player === computer) {
-    return "Stalemate";
-  } else if (PlayerHasWon(player, computer)) {
-    return `You Win! ${capitalizeFirstLetterInWord(player)} beats ${capitalizeFirstLetterInWord(computer)}`;
-  } else if (ComputerHasWon(player, computer)) {
-    return `You Lose! ${capitalizeFirstLetterInWord(computer)} beats ${capitalizeFirstLetterInWord(player)}`;
-  }
-}
-
-/**
- * Capitalize the first letter in a word
- * @param {string} word The word whose first letter is to be capitalized
- * @returns A new string containing the word with its first letter capitalized
- */
-function capitalizeFirstLetterInWord(word) {
-  return word[0].toUpperCase() + word.slice(1, word.length);
-}
+import { PlayRound } from "./PlayRound.js";
 
 let playerScore = 0;
 let computerScore = 0;
@@ -40,19 +13,19 @@ function game() {
 
   const rockButton = document.querySelector(".btn-rock");
   rockButton.addEventListener("click", () => {
-    div.textContent = playRound("rock", GetComputerChoice());
+    div.textContent = PlayRound("rock", GetComputerChoice());
     score.textContent = `Player Score = ${playerScore}, Computer Score = ${computerScore}`;
   });
 
   const paperButton = document.querySelector(".btn-paper");
   paperButton.addEventListener("click", () => {
-    div.textContent = playRound("paper", GetComputerChoice());
+    div.textContent = PlayRound("paper", GetComputerChoice());
     score.textContent = `Player Score = ${playerScore}, Computer Score = ${computerScore}`;
   });
 
   const scissorsButton = document.querySelector(".btn-scissors");
   scissorsButton.addEventListener("click", () => {
-    div.textContent = playRound("scissors", GetComputerChoice());
+    div.textContent = PlayRound("scissors", GetComputerChoice());
     score.textContent = `Player Score = ${playerScore}, Computer Score = ${computerScore}`;
   });
 
